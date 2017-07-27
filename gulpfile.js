46c85222838e4f16a0510742bba8b07e0096f9c4
Derek Neuts <dcneuts@gmail.com>
2017-07-26 22:54:50 -0700

    Combined multiple JS scripts into one script using gulp and gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 "use strict";
 
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 
-gulp.task("hello", function () {
-	console.log("Hello!");
+// use gulp to combine multiple JS scripts into one file
+gulp.task("concatScripts", function () {
+	gulp.src([
+		'js/jquery.js',
+		'js/sticky/jquery.sticky.js',
+		'js/main.js'])
+	    .pipe(concat("app.js"))
+	    // dest method writes to disk
+		.pipe(gulp.dest("js"))
 });
 
 gulp.task("default", ["hello"], function () {
