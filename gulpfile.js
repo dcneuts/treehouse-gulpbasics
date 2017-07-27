d0834d76989fd1186bbdb08ef6bd508eaaf001b2
Derek Neuts <dcneuts@gmail.com>
2017-07-27 00:30:40 -0700

    Added gulp-sass and experimented with minification using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
 
 // use gulp to combine multiple JS scripts into one file
 gulp.task("concatScripts", function () {
@@ -19,8 +21,10 @@ gulp.task("concatScripts", function () {
 gulp.task("minifyScripts", function () {
 	// calling uglify to minify app.js and then dump what's in mem to disk
 	// placing the output in the JS folder
-	gulp.src("js/app.js")
-		.pipe(uglify()).pipe(gulp.dest('js'));
+	gulp.src("js/app.js").pipe(uglify())
+	    //needed to add gulp rename to not overwrite main js
+	    .pipe(rename('app.min.js'))
+	    .pipe(gulp.dest("js"));
 });
 
 gulp.task("default", ["hello"], function () {
